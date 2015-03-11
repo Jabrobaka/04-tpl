fd1db00828b8f12f1ebce75620810bd2c6dfde6b
Jabrobaka <alexander.kulikov.ru@gmail.com>
2015-03-11 21:10:13 +0500

    LineBlocks для парсинга блоков (цифры в линиях)

---
+++ JapaneseCrossword/LineBlocks.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JapaneseCrossword
+{
+    public static class LineBlocks
+    {
+
+        public static int[] FromString(string info, int lineLength)
+        {
+            var figures = info
+                .Trim()
+                .Split(' ')
+                .Select(Int32.Parse)
+                .ToArray();
+
+            if (figures.Sum() > lineLength)
+            {
+                throw new IncorrectCrosswordException();
+            }
+
+            return figures.ToArray();
+        }
+    }
+    internal class IncorrectCrosswordException : Exception
+    {
+    }
+}
